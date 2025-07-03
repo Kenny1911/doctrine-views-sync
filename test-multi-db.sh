3fd020ed8bf7a14b9b79612dbb48f580e137b072
Kenny1911 <o-muzyka@mail.ru>
2025-07-03 23:40:29 +0400

    Use only port number in POSTGRES_PORT env var

--- test-multi-db.sh
+++ test-multi-db.sh
@@ -9,7 +9,7 @@ composer run phpunit
 # Run Postgres
 echo 'Run tests for Postgres'
 POSTGRES_PORT="${POSTGRES_PORT:-5432}"
-POSTGRES_PORT="127.0.0.1:${POSTGRES_PORT:?}" ${DOCKER_COMPOSE} up -d postgres || exit $?
+POSTGRES_PORT="${POSTGRES_PORT:?}" ${DOCKER_COMPOSE} up -d postgres || exit $?
 sleep 5 # Wait starting db
 
 DATABASE_URL="pdo-pgsql://postgres:123@127.0.0.1:${POSTGRES_PORT:?}/views-sync" composer run phpunit || {
