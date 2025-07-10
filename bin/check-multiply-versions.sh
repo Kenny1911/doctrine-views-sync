2ce624625080bc032f5f604ca8738a69fafafa0b
Kenny1911 <o-muzyka@mail.ru>
2025-07-10 23:52:35 +0400

    Constrain doctrine/dbal version to <4.3 in check-multiply-versions.sh

--- bin/check-multiply-versions.sh
+++ bin/check-multiply-versions.sh
@@ -3,7 +3,7 @@
 WORKING_DIR="$(dirname $(dirname $(realpath $0)))"
 cd "${WORKING_DIR}"
 
-for doctrine_dbal_ver in '^3.0' '^4.0'; do
+for doctrine_dbal_ver in '^3.0' '^4.0 <4.3'; do
   for doctrine_persistence in '^3.1' '^4.0'; do
     for symfony_console_version in '^5.4' '^6.0' '^7.0'; do
       composer install &&
